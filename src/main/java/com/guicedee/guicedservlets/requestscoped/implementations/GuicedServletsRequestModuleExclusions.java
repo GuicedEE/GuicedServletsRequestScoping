f7e842079fe00003be4c528cb4f10281e2ff0b19
GedMarc <ged_marc@hotmial.com>
2020-03-08 17:00:08 +0200

    Remove Jar syntax as JDK 8 is no longer supported

--- src/main/java/com/guicedee/guicedservlets/requestscoped/implementations/GuicedServletsRequestModuleExclusions.java
+++ src/main/java/com/guicedee/guicedservlets/requestscoped/implementations/GuicedServletsRequestModuleExclusions.java
@@ -1,6 +1,5 @@
 package com.guicedee.guicedservlets.requestscoped.implementations;
 
-import com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions;
 import com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 
 import javax.validation.constraints.NotNull;
@@ -8,17 +7,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class GuicedServletsRequestModuleExclusions
-		implements IGuiceScanModuleExclusions<GuicedServletsRequestModuleExclusions>,
-				           IGuiceScanJarExclusions<GuicedServletsRequestModuleExclusions>
+		implements IGuiceScanModuleExclusions<GuicedServletsRequestModuleExclusions>
 {
-	@Override
-	public @NotNull Set<String> excludeJars()
-	{
-		Set<String> strings = new HashSet<>();
-		strings.add("guiced-servlets-request-scoper-*");
-		return strings;
-	}
-
 	@Override
 	public @NotNull Set<String> excludeModules()
 	{

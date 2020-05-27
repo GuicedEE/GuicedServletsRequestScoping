0664cb91e4e8c6e07d3b14f8fea55cf57686b8ef
GedMarc <ged_marc@hotmail.com>
2020-05-27 03:06:54 +0200

    Updates

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,13 +1,7 @@
 module com.guicedee.guicedservlets.requestscoped {
-	requires com.google.guice;
-	requires java.servlet;
-	requires com.google.guice.extensions.persist;
-	requires com.guicedee.guicedinjection;
+
 	requires com.guicedee.guicedservlets;
 	requires com.guicedee.guicedpersistence;
-	requires java.logging;
-	requires com.guicedee.logmaster;
-	requires java.validation;
 
 	exports com.guicedee.guicedservlets.requestscoped;
 

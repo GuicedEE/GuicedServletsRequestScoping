41dd58d7995c421601bf5e9f368030ba3c30e1d9
GedMarc <marc.magon@gmail.com>
2024-04-13 15:33:04 +0200

    Add Vert.x, MP Config use raw injection, JTA Updates, Remove annotation and use persistence unit name.

--- src/main/java/com/guicedee/guicedservlets/requestscoped/OnCallScopeStart.java
+++ src/main/java/com/guicedee/guicedservlets/requestscoped/OnCallScopeStart.java
@@ -14,45 +14,35 @@ import java.util.logging.*;
 
 public class OnCallScopeStart implements IOnCallScopeEnter<OnCallScopeStart>
 {
-	@Override
-	public void onScopeEnter(Scope scope)
-	{
-		List<Map.Entry<Class<? extends Annotation>, com.google.inject.Module>> collect = new ArrayList(PersistenceServicesModule
-				                                                                                               .getModules()
-				                                                                                               .entrySet());
-		if (!collect.isEmpty())
-		{
-			for (Map.Entry<Class<? extends Annotation>, Module> entry : collect)
-			{
-				Class<? extends Annotation> key = entry.getKey();
-				PersistService persistServiceKey = IGuiceContext.get(Key.get(PersistService.class, key));
-				UnitOfWork unitOfWork = IGuiceContext.get(Key.get(UnitOfWork.class, key));
-				try
-				{
-					persistServiceKey.start();
-					unitOfWork.begin();
-				}
-				catch (Exception e)
-				{
-					Logger
-							.getLogger(getClass().getName())
-							.log(Level.SEVERE, "Unable to start persist service for servlet call", e);
-				}
-			}
-		}
-	}
-	
-	/**
-	 * Integer min + 100
-	 * <p>
-	 * Always try to run before any other call scope action
-	 *
-	 * @return
-	 */
-	@Override
-	public Integer sortOrder()
-	{
-		return Integer.MIN_VALUE + 100;
-	}
-	
+    @Override
+    public void onScopeEnter(Scope scope)
+    {
+        PersistService persistServiceKey = IGuiceContext.get(Key.get(PersistService.class));
+        UnitOfWork unitOfWork = IGuiceContext.get(Key.get(UnitOfWork.class));
+        try
+        {
+            persistServiceKey.start();
+            unitOfWork.begin();
+        }
+        catch (Exception e)
+        {
+            Logger
+                    .getLogger(getClass().getName())
+                    .log(Level.SEVERE, "Unable to start persist service for servlet call", e);
+        }
+    }
+
+    /**
+     * Integer min + 100
+     * <p>
+     * Always try to run before any other call scope action
+     *
+     * @return
+     */
+    @Override
+    public Integer sortOrder()
+    {
+        return Integer.MIN_VALUE + 100;
+    }
+
 }
--- src/main/java/com/guicedee/guicedservlets/requestscoped/SiteRequestScopedFilterBinder.java
+++ src/main/java/com/guicedee/guicedservlets/requestscoped/SiteRequestScopedFilterBinder.java
@@ -1,59 +1,26 @@
 package com.guicedee.guicedservlets.requestscoped;
 
-import com.google.inject.Module;
-import com.google.inject.servlet.*;
-import com.guicedee.guicedinjection.interfaces.*;
-import com.guicedee.guicedpersistence.services.*;
-import com.guicedee.guicedservlets.services.*;
-import lombok.extern.java.*;
+import com.google.inject.servlet.ServletModule;
+import com.guicedee.guicedinjection.interfaces.IGuiceModule;
+import lombok.extern.java.Log;
 
-import java.lang.annotation.*;
-import java.util.*;
-import java.util.logging.*;
+import java.lang.annotation.Annotation;
+import java.util.HashSet;
+import java.util.Set;
 
 @Log
 public class SiteRequestScopedFilterBinder extends ServletModule implements IGuiceModule<SiteRequestScopedFilterBinder>
 {
-	/**
-	 * A set of annotations to not assign request scope transactions to
-	 */
-	private static final Set<Class<? extends Annotation>> excludedAnnotations = new HashSet<>();
-	
-	/**
-	 * Method getExcludedAnnotations returns the excludedAnnotations of this SiteRequestScopedFilterBinder object.
-	 * <p>
-	 * A set of annotations to not assign request scope transactions to
-	 *
-	 * @return the excludedAnnotations (type Set Class ? extends Annotation ) of this SiteRequestScopedFilterBinder object.
-	 */
-	public static Set<Class<? extends Annotation>> getExcludedAnnotations()
-	{
-		return SiteRequestScopedFilterBinder.excludedAnnotations;
-	}
-	
-	
-	@Override
-	protected void configureServlets()
-	{
-		SiteRequestScopedFilterBinder.log.config("Loading Request Scope Transactions");
-		List<Map.Entry<Class<? extends Annotation>, com.google.inject.Module>> collect = new ArrayList(PersistenceServicesModule
-				                                                                                               .getModules()
-				                                                                                               .entrySet());
-		if (!collect.isEmpty())
-		{
-			for (Map.Entry<Class<? extends Annotation>, Module> entry : collect)
-			{
-				PersistFilter filter = new PersistFilter(entry.getKey());
-				filter("/*").through(filter);
-				SiteRequestScopedFilterBinder.log.config("Request Scoped Filter Added, Initial Entry @" + entry.getKey());
-				log.log(Level.CONFIG, "Started " + collect.size() + " Request Scope on Module - " + entry);
-			}
-		}
-	}
-	
-	@Override
-	public Integer sortOrder()
-	{
-		return 150;
-	}
+    @Override
+    protected void configureServlets()
+    {
+        SiteRequestScopedFilterBinder.log.config("Loading Request Scope Transactions");
+        filter("/*").through(PersistFilter.class);
+    }
+
+    @Override
+    public Integer sortOrder()
+    {
+        return 150;
+    }
 }
